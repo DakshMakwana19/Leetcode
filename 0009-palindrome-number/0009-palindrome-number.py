44d5119c474d1b96f5729cc7358753467e2ca103
Daksh Makwana <dakshmakwana1912@gmail.com>
2026-07-11 22:30:29 +0530

    Time: 8 ms (71.48%), Space: 12.4 MB (17.32%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x=str(x)
+        for i in range(len(x)//2):
+            if x[i]!=x[-i-1]:
+                return False
+                break
+        
+        return True
+
+    
+        
+             
+       
+
+        
+        """
+        :type x: int
+        :rtype: bool
+        """
+        

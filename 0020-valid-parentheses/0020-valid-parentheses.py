9a550943ae0ae76e8e6d5d127366d9925ade1954
Daksh Makwana <dakshmakwana1912@gmail.com>
2026-08-01 16:31:30 +0530

    Time: 7 ms (12.76%), Space: 12.5 MB (71.05%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        for ch in s:
+            if ch =="(" or ch=="{" or ch=="[":
+                stack.append(ch)
+            else:
+                if len(stack)==0:
+                    return False
+                top=stack.pop()
+                if (ch==")" and top!="(") or (ch=="]" and top!="[") or (ch=="}" and top!="{"):
+                    return False
+        return len(stack)==0 
+
+        

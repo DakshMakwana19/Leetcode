c10cf356b6e3a31f6c0502db24d46ffecb968844
Daksh Makwana <dakshmakwana1912@gmail.com>
2026-08-04 21:35:20 +0530

    Time: 7 ms (52.53%), Space: 12.5 MB (0.46%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def findMissingElements(self, nums):
         
-        sety=set(nums)
+        nums.sort()
         
         minu=min(nums)
         maxu=max(nums)
         ans=[]
-        for i in range(minu,maxu+1):
-            if i not in sety:
-                ans.append(i)
+        for num in range(minu,maxu+1):
+            if num not in nums:
+                ans.append(num)
         return ans
 
            

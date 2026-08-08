ec60d043cc78a075b970984e1e3b1d441eaad275
Daksh Makwana <dakshmakwana1912@gmail.com>
2026-08-08 23:43:14 +0530

    Time: 11 ms (9.41%), Space: 12.4 MB (64.58%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        level = 0
+        result = []
+
+        for ch in s:
+            if ch == "(":
+                if level > 0:
+                    result += ch
+                level += 1
+
+            elif ch == ")":
+                level-=1
+                if level > 0:
+                    result += ch
+                
+
+        return "".join(result)

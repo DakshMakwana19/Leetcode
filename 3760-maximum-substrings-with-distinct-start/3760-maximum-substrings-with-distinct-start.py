83089da45884ab0d4eed6707899ddc4733e541e2
Daksh Makwana <dakshmakwana1912@gmail.com>
2026-08-11 23:35:52 +0530

    Time: 21 ms (89.92%), Space: 12.8 MB (97.67%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
@@ -1,9 +1,4 @@
 class Solution(object):
     def maxDistinct(self, s):
-        hash={}
-        for ch in s:
-            if s not in hash:
-                hash[ch]=0
-            hash[ch]+=1
-        return len(hash)
+        return len(set(s))
         
